27d529384e7d5e2cad80174a5003040f310c7e46
jakedahn <jake@markupisart.com>
2014-10-19 17:22:36 -0700

    fixing long-broken update function

--- service.js
+++ service.js
@@ -2,5 +2,5 @@ $(function(){
   interestingness = new Flickr({key: "1031f6201017777f7b33cee31f9067c3"});
   interestingness.request();
   interestingness.update();
-  setInterval(interestingness.update(), 43000); // 12 hours refresh
+  setInterval(interestingness.update, 60*60*1000);
 });
